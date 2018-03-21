7cfc797f961b7ae84a4632e56937d7fc0bc85715
DESKTOP-2G9T0U6\kime <12589090@sina.com>
2018-03-21 17:37:59 +0800

    树形签核

--- OperationalApplicationSystem/src/main/java/com/kime/model/Approve.java
+++ OperationalApplicationSystem/src/main/java/com/kime/model/Approve.java
@@ -1,10 +1,16 @@
 package com.kime.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity @Table(name = "t_approve")
 public class Approve {
 	@Id
 	@GeneratedValue(generator="uuid")
