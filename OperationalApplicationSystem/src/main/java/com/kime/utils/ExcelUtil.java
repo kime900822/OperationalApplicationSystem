09b6b7c76fa6543e0f47c3cfcade596c5de0f65c
DESKTOP-2G9T0U6\kime <12589090@sina.com>
2018-03-30 14:24:37 +0800

    打印优化

--- OperationalApplicationSystem/src/main/java/com/kime/utils/ExcelUtil.java
+++ OperationalApplicationSystem/src/main/java/com/kime/utils/ExcelUtil.java
@@ -182,44 +182,7 @@ public static <T>ByteArrayOutputStream  exportExcel(String title, Class class1, 
 			//宽度自适应
 			sheet.setColumnWidth(i, fs[i].getName().getBytes().length*2*256);
 	}
-    // 生成一个样式
-    HSSFCellStyle style = workbook.createCellStyle();
-    // 设置这些样式
-    style.setFillForegroundColor(HSSFColor.GREY_25_PERCENT.index);
-    style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-    style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-    style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-    style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-    style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-    style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
-    // 生成一个字体
-    HSSFFont font = workbook.createFont();
-    font.setColor(HSSFColor.BLACK.index);
-    font.setFontHeightInPoints((short) 10);
-    font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-    // 把字体应用到当前的样式
-    style.setFont(font);
-    // 生成并设置另一个样式
-    HSSFCellStyle style2 = workbook.createCellStyle();
-    style2.setFillForegroundColor(HSSFColor.WHITE.index);
-    style2.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-    style2.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-    style2.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-    style2.setBorderRight(HSSFCellStyle.BORDER_THIN);
-    style2.setBorderTop(HSSFCellStyle.BORDER_THIN);
-    style2.setAlignment(HSSFCellStyle.ALIGN_CENTER);
-    style2.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
-    // 生成另一个字体
-    HSSFFont font2 = workbook.createFont();
-    font2.setBoldweight(HSSFFont.BOLDWEIGHT_NORMAL);
-    // 把字体应用到当前的样式
-    style2.setFont(font2);
 
-    //设置单元格个税文本类型
-    HSSFDataFormat format = workbook.createDataFormat(); 
-    style.setDataFormat(format.getFormat("@")); 
-    style2.setDataFormat(format.getFormat("@")); 
-    
     
     // 声明一个画图的顶级管理器
     HSSFPatriarch patriarch = sheet.createDrawingPatriarch();
@@ -232,19 +195,19 @@ public static <T>ByteArrayOutputStream  exportExcel(String title, Class class1, 
     
 
     // 产生表格标题行
+    CellStyle cellStyle=getHeaderCellStyle(workbook);
     HSSFRow row = sheet.createRow(0);
     for (int i = 0; i < headers.length; i++) {
         HSSFCell cell = row.createCell(i);
-        cell.setCellStyle(style);
+        cell.setCellStyle(cellStyle);
         HSSFRichTextString text = new HSSFRichTextString(headers[i]);
         cell.setCellValue(text);
     }
 
     // 遍历集合数据，产生数据行
+    cellStyle=getColCellStyle(workbook);
     Iterator<T> it = dataset.iterator();
     int index = 0;
-    HSSFFont font3 = workbook.createFont();
-    font3.setColor(HSSFColor.BLACK.index);
     while (it.hasNext()) {
         index++;
         row = sheet.createRow(index);
@@ -253,7 +216,7 @@ public static <T>ByteArrayOutputStream  exportExcel(String title, Class class1, 
         Field[] fields = t.getClass().getDeclaredFields();
         for (int i = 0; i < fields.length; i++) {
             HSSFCell cell = row.createCell(i);
-            cell.setCellStyle(style2);
+            cell.setCellStyle(cellStyle);
             Field field = fields[i];
             String fieldName = field.getName();
             String getMethodName = "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
@@ -294,8 +257,6 @@ public static <T>ByteArrayOutputStream  exportExcel(String title, Class class1, 
                 // 如果不是图片数据，就利用正则表达式判断textValue是否全部由数字组成
                 if (textValue != null) {
                     HSSFRichTextString richString = new HSSFRichTextString(textValue);
-
-                    richString.applyFont(font3);
                     cell.setCellValue(richString);
 /*                    Pattern p = Pattern.compile("^\\d+(\\.\\d+)?$");
                     Matcher matcher = p.matcher(textValue);
@@ -360,45 +321,6 @@ public static <T>ByteArrayOutputStream  exportExcel(String title, Class class1, 
 		aligns[i]=lColumns.get(i).getAlign();
 		sheet.setColumnWidth(i,Integer.parseInt(lColumns.get(i).getWidth())*256/8);
 	}
-	
-	
-    // 生成一个样式
-    HSSFCellStyle style = workbook.createCellStyle();
-    // 设置这些样式
-    style.setFillForegroundColor(HSSFColor.GREY_25_PERCENT.index);
-    style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-    style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-    style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-    style.setBorderRight(HSSFCellStyle.BORDER_THIN);
-    style.setBorderTop(HSSFCellStyle.BORDER_THIN);
-    style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
-    // 生成一个字体
-    HSSFFont font = workbook.createFont();
-    font.setColor(HSSFColor.BLACK.index);
-    font.setFontHeightInPoints((short) 10);
-    font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
-    // 把字体应用到当前的样式
-    style.setFont(font);
-    // 生成并设置另一个样式
-    HSSFCellStyle style2 = workbook.createCellStyle();
-    style2.setFillForegroundColor(HSSFColor.WHITE.index);
-    style2.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-    style2.setBorderBottom(HSSFCellStyle.BORDER_THIN);
-    style2.setBorderLeft(HSSFCellStyle.BORDER_THIN);
-    style2.setBorderRight(HSSFCellStyle.BORDER_THIN);
-    style2.setBorderTop(HSSFCellStyle.BORDER_THIN);
-    style2.setAlignment(HSSFCellStyle.ALIGN_CENTER);
-    style2.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
-    // 生成另一个字体
-    HSSFFont font2 = workbook.createFont();
-    font2.setBoldweight(HSSFFont.BOLDWEIGHT_NORMAL);
-    // 把字体应用到当前的样式
-    style2.setFont(font2);
-
-    //设置单元格个税文本类型
-    HSSFDataFormat format = workbook.createDataFormat(); 
-    style.setDataFormat(format.getFormat("@")); 
-    style2.setDataFormat(format.getFormat("@")); 
     
     
     // 声明一个画图的顶级管理器
@@ -410,29 +332,38 @@ public static <T>ByteArrayOutputStream  exportExcel(String title, Class class1, 
     // 设置注释作者，当鼠标移动到单元格上是可以在状态栏中看到该内容.
     comment.setAuthor("Analysis");
     
-
-    // 产生表格标题行
+    //表名
     HSSFRow row = sheet.createRow(0);
+    HSSFCell cell = row.createCell(0);
+    cell.setCellStyle(getTitleCellStyle(workbook));
+    HSSFRichTextString text = new HSSFRichTextString(title);
+    cell.setCellValue(text);
+    //合并
+    CellRangeAddress cra =new CellRangeAddress(0, 0, 0, lColumns.size()-1);
+    sheet.addMergedRegion(cra);  
+    
+    // 产生表格标题行
+    CellStyle cellStyle=getHeaderCellStyle(workbook);
+    row = sheet.createRow(1);
     for (int i = 0; i < headers.length; i++) {
-        HSSFCell cell = row.createCell(i);
-        cell.setCellStyle(getHeaderCellStyle(workbook));
-        HSSFRichTextString text = new HSSFRichTextString(headers[i]);
+        cell = row.createCell(i);
+        cell.setCellStyle(cellStyle);
+        text = new HSSFRichTextString(headers[i]);
         cell.setCellValue(text);
     }
 
     // 遍历集合数据，产生数据行
+    cellStyle=getColCellStyle(workbook);
     Iterator<T> it = dataset.iterator();
-    int index = 0;
-    HSSFFont font3 = workbook.createFont();
-    font3.setColor(HSSFColor.BLACK.index);
+    int index = 1;
     while (it.hasNext()) {
         index++;
         row = sheet.createRow(index);
         T t = (T) it.next();
 
         for (int i = 0; i < headers.length; i++) {
-            HSSFCell cell = row.createCell(i);
-            cell.setCellStyle(style2);
+            cell = row.createCell(i);
+            cell.setCellStyle(cellStyle);
             String fieldName = fields[i];
             String getMethodName = "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
             try {
@@ -472,20 +403,7 @@ public static <T>ByteArrayOutputStream  exportExcel(String title, Class class1, 
                 // 如果不是图片数据，就利用正则表达式判断textValue是否全部由数字组成
                 if (textValue != null) {
                     HSSFRichTextString richString = new HSSFRichTextString(textValue);
-                    richString.applyFont(font3);
                     cell.setCellValue(richString);
-/*                    Pattern p = Pattern.compile("^\\d+(\\.\\d+)?$");
-                    Matcher matcher = p.matcher(textValue);
-                    if (matcher.matches()) {
-                        // 是数字当作double处理
-                        cell.setCellValue(Double.parseDouble(textValue));
-                    } else {
-                        HSSFRichTextString richString = new HSSFRichTextString(textValue);
-                        HSSFFont font3 = workbook.createFont();
-                        font3.setColor(HSSFColor.BLUE.index);
-                        richString.applyFont(font3);
-                        cell.setCellValue(richString);
-                    }*/
                 }
             } catch (SecurityException e) {
                 e.printStackTrace();
@@ -581,6 +499,28 @@ public static  List FileToList(Class c,File file,String first,String filename,in
 
 	}
 
+	private static HSSFCellStyle getColCellStyle(HSSFWorkbook wb) {
+		
+	    HSSFCellStyle style = wb.createCellStyle();
+	    style.setFillForegroundColor(HSSFColor.WHITE.index);
+	    style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+	    style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+	    style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
+	    style.setBorderRight(HSSFCellStyle.BORDER_THIN);
+	    style.setBorderTop(HSSFCellStyle.BORDER_THIN);
+	    style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+	    style.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+	    HSSFDataFormat format = wb.createDataFormat(); 
+	    style.setDataFormat(format.getFormat("@")); 
+	    // 生成另一个字体
+	    HSSFFont font = wb.createFont();
+	    font.setBoldweight(HSSFFont.BOLDWEIGHT_NORMAL);
+	    // 把字体应用到当前的样式
+	    style.setFont(font);
+	    return style;
+		
+		
+	}
 	
 	
 	private static HSSFCellStyle getHeaderCellStyle(HSSFWorkbook wb) {
@@ -597,6 +537,26 @@ public static  List FileToList(Class c,File file,String first,String filename,in
 		return cellStyle;
 	}
 
+	private static HSSFCellStyle getTitleCellStyle(HSSFWorkbook wb) {
+
+		HSSFCellStyle cellStyle = wb.createCellStyle();
+		cellStyle.setFont(getHeaderFont(wb));
+		cellStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+		cellStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+
+		cellStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN); // 下边框
+		cellStyle.setBorderLeft(HSSFCellStyle.BORDER_THIN);// 左边框
+		cellStyle.setBorderTop(HSSFCellStyle.BORDER_THIN);// 上边框
+		cellStyle.setBorderRight(HSSFCellStyle.BORDER_THIN);// 右边框
+		
+		HSSFFont font = wb.createFont();
+		font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);// 字体加粗
+		font.setFontName("Times New Roman");
+		font.setFontHeightInPoints((short) 14);
+		font.setBold(true);
+		
+		return cellStyle;
+	}
 	
 	private static HSSFFont getHeaderFont(HSSFWorkbook wb) {
 
@@ -604,6 +564,7 @@ public static  List FileToList(Class c,File file,String first,String filename,in
 		font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);// 字体加粗
 		font.setFontName("Times New Roman");
 		font.setFontHeightInPoints((short) 8);
+		font.setBold(true);
 		return font;
 	}
 
