e8a886423a07be31ebb6bc4c244fca062abec901
kime <12589090@sina.com>
2018-10-22 16:44:48 +0800

    出差预申请有预付款的。自动生成付款申请单

--- OperationalApplicationSystem/src/main/java/com/sign/biz/impl/PaymentVisitBIZImpl.java
+++ OperationalApplicationSystem/src/main/java/com/sign/biz/impl/PaymentVisitBIZImpl.java
@@ -1,5 +1,6 @@
 package com.sign.biz.impl;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -312,52 +313,66 @@ public class PaymentVisitBIZImpl extends BizBase implements PaymentVisitBIZ {
 
 	@Override
 	public void buildPayment(PaymentVisit paymentVisit) throws Exception {
-		Payment payment=new Payment();
-		payment.setApplicationDate(paymentVisit.getApplicantDate());
-		payment.setState(PaymentHelp.SAVEPAYMENT);
-		payment.setVisitId(paymentVisit.getId());
-		payment.setPaymentSubject(PaymentHelp.TRAVEL);
-		payment.setUID(paymentVisit.getuId());
-		payment.setUName(paymentVisit.getuName());
-		payment.setUsageDescription(paymentVisit.getVisitDateFrom()+" "+paymentVisit.getVisitDateTo()+" "+paymentVisit.getVisitDetailPlace()+" "+paymentVisit.getVisitPurpose());
-		payment.setCurrency_1(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
-		payment.setCurrency_2(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
-		payment.setCurrency_3(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
-		payment.setCurrency_4(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
-		payment.setCurrency_5(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
-		payment.setCurrency_6(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
-		double amount_1=0;
-		double amount_2=0;
-		double amount_3=0;
-		double amount_4=0;
-		double amount_5=0;
-		double amount_6=0;
-		
-		for (PaymentVisitBusinessTrip paymentVisitBusinessTrip : paymentVisit.getBusinessTrips()) {
-			amount_1+=paymentVisitBusinessTrip.getLandwayTotal();
-			amount_2+=paymentVisitBusinessTrip.getRoadToilVAT();
-			amount_3+=paymentVisitBusinessTrip.getHotelWithoutVAT();
-			amount_4+=paymentVisitBusinessTrip.getHotelVAT();
-			amount_5+=paymentVisitBusinessTrip.getMealTotal();
-			amount_6+=paymentVisitBusinessTrip.getAirTicket();
-		}
-		payment.setAmount_1(amount_1==0?"":String.valueOf(amount_1));
-		payment.setAmount_2(amount_2==0?"":String.valueOf(amount_2));
-		payment.setAmount_3(amount_3==0?"":String.valueOf(amount_3));
-		payment.setAmount_4(amount_4==0?"":String.valueOf(amount_4));
-		payment.setAmount_5(amount_5==0?"":String.valueOf(amount_5));
-		payment.setAmount_1(amount_6==0?"":String.valueOf(amount_6));
-		
-		payment.setDateTemp(CommonUtil.getDateTemp());
-		payment.setId(UUID.randomUUID().toString().replaceAll("-", ""));
-		
-		List<Dict> lDicts=dictDAO.query(" where key='"+payment.getPaymentSubject()+"'");
-		if (!"".equals(lDicts.get(0).getValue())&&lDicts.get(0).getValue()!=null) {
-			patmentDAO.save(payment);
-			logUtil.logInfo("自动生成付款申请单:"+payment.getId());
-		}else{
-			logUtil.logInfo("自动付款申异常:未维护对应财务人员");
-			throw new Exception("自动付款申异常:未维护对应财务人员");
+		for (PaymentVisitEmployee employee : paymentVisit.getEmployees()) {
+			if (employee.getAdvanceAmount().compareTo(BigDecimal.ZERO)==1) {
+				Payment payment=new Payment();
+				List luser=userDAO.query(" where uid='"+employee.getEmployeeNo()+"'");
+				if (luser.size()==0) {
+					logUtil.logInfo("自动付款申异常:出差申请人员未找到:"+employee.getEmployeeNo());
+					throw new Exception("自动付款申异常:出差申请人员未找到:"+employee.getEmployeeNo());
+				}
+				User user=(User) luser.get(0);
+				payment.setApplicationDate(paymentVisit.getApplicantDate());
+				payment.setState(PaymentHelp.SAVEPAYMENT);
+				payment.setVisitId(paymentVisit.getId());
+				payment.setPaymentSubject(PaymentHelp.TRAVEL);
+				payment.setUID(user.getUid());
+				payment.setUName(user.getName());
+				payment.setDepartmentID(user.getDid());
+				payment.setDepartmentName(user.getDepartment().getName());
+				payment.setDeptManagerID(user.getDepartment().getUid());
+				payment.setDeptManager(user.getDepartment().getName());
+				payment.setUsageDescription(paymentVisit.getVisitDateFrom()+" "+paymentVisit.getVisitDateTo()+" "+paymentVisit.getVisitDetailPlace()+" "+paymentVisit.getVisitPurpose());
+				payment.setCurrency_1(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
+				payment.setCurrency_2(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
+				payment.setCurrency_3(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
+				payment.setCurrency_4(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
+				payment.setCurrency_5(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
+				payment.setCurrency_6(paymentVisit.getBusinessTrips().size()==0?paymentVisit.getBusinessTrips().get(0).getCurrency():"");
+				double amount_1=0;
+				double amount_2=0;
+				double amount_3=0;
+				double amount_4=0;
+				double amount_5=0;
+				double amount_6=0;
+				
+				for (PaymentVisitBusinessTrip paymentVisitBusinessTrip : paymentVisit.getBusinessTrips()) {
+					amount_1+=paymentVisitBusinessTrip.getLandwayTotal();
+					amount_2+=paymentVisitBusinessTrip.getRoadToilVAT();
+					amount_3+=paymentVisitBusinessTrip.getHotelWithoutVAT();
+					amount_4+=paymentVisitBusinessTrip.getHotelVAT();
+					amount_5+=paymentVisitBusinessTrip.getMealTotal();
+					amount_6+=paymentVisitBusinessTrip.getAirTicket();
+				}
+				payment.setAmount_1(amount_1==0?"":String.valueOf(amount_1));
+				payment.setAmount_2(amount_2==0?"":String.valueOf(amount_2));
+				payment.setAmount_3(amount_3==0?"":String.valueOf(amount_3));
+				payment.setAmount_4(amount_4==0?"":String.valueOf(amount_4));
+				payment.setAmount_5(amount_5==0?"":String.valueOf(amount_5));
+				payment.setAmount_1(amount_6==0?"":String.valueOf(amount_6));
+				
+				payment.setDateTemp(CommonUtil.getDateTemp());
+				payment.setId(UUID.randomUUID().toString().replaceAll("-", ""));
+				
+				List<Dict> lDicts=dictDAO.query(" where key='"+payment.getPaymentSubject()+"'");
+				if (!"".equals(lDicts.get(0).getValue())&&lDicts.get(0).getValue()!=null) {
+					patmentDAO.save(payment);
+					logUtil.logInfo("自动生成付款申请单:"+payment.getId());
+				}else{
+					logUtil.logInfo("自动付款申异常:未维护对应财务人员");
+					throw new Exception("自动付款申异常:未维护对应财务人员");
+				}
+			}
 		}
 	}
 
