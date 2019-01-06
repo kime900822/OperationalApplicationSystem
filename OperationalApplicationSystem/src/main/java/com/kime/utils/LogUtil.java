13d7a017d179b63acf49d113f81bd58667b58fb2
赵舟浩 <>
2019-01-06 12:41:57 +0800

    日志工具类修改

--- OperationalApplicationSystem/src/main/java/com/kime/utils/LogUtil.java
+++ OperationalApplicationSystem/src/main/java/com/kime/utils/LogUtil.java
@@ -20,52 +20,107 @@ public class LogUtil  {
 	  private static Logger log = Logger.getLogger(LogUtil.class);
 	  
 	  public void logDebug(String message){
-		  User user=(User) session.getAttribute("user");
-		  String ip = request.getRemoteAddr();
+		  User user;
+		  String ip;
+		if (session==null) {
+			user=null;
+			ip=null;
+		}else {
+			user = (User) session.getAttribute("user");
+			ip = request.getRemoteAddr();
+		}
 		  if (user!=null) {
 			  log.debug(" IP:"+ip+" 工号:"+user.getUid()+" 操作:" +message);
+		  }else{
+			  log.error(" IP:"+ip+"  操作: "+message);
 		  }
 		  
 	  }
 	  
 	  public  void logInfo(String message){
-		  User user=(User) session.getAttribute("user");
-		  String ip = request.getRemoteAddr();
+		  User user;
+		  String ip;
+		if (session==null) {
+			user=null;
+			ip=null;
+		}else {
+			user = (User) session.getAttribute("user");
+			ip = request.getRemoteAddr();
+		}
 		  if (user!=null) {
 			  log.info(" IP:"+ip+" 工号:"+user.getUid()+" 操作:" +message);
+		  }else{
+			  log.error(" IP:"+ip+"  操作: "+message);
 		  }
 	  }
 	  
 	  public  void logError(String message){
-		  User user=(User) session.getAttribute("user");
-		  String ip = request.getRemoteAddr();
+		  User user;
+		  String ip;
+		if (session==null) {
+			user=null;
+			ip=null;
+		}else {
+			user = (User) session.getAttribute("user");
+			ip = request.getRemoteAddr();
+		}
 		  if (user!=null) {
 			  log.error(" IP:"+ip+" 工号:"+user.getUid()+" 操作:" +message);
+		  }else{
+			  log.error(" IP:"+ip+"  操作: "+message);
 		  }
 	  }
 	  
 	  public void logDebug(String title,String message){
-		  User user=(User) session.getAttribute("user");
-		  String ip = request.getRemoteAddr();
+		  User user;
+		  String ip;
+		if (session==null) {
+			user=null;
+			ip=null;
+		}else {
+			user = (User) session.getAttribute("user");
+			ip = request.getRemoteAddr();
+		}
 		  if (user!=null) {
 			  log.debug(" IP:"+ip+" 工号:"+user.getUid()+" 操作:" +title+":"+message);
+		  }else{
+			  log.error(" IP:"+ip+"  操作:" +title+":"+message);
 		  }
 		  
 	  }
 	  
 	  public  void logInfo(String title,String message){
-		  User user=(User) session.getAttribute("user");
-		  String ip = request.getRemoteAddr();
+		  User user;
+		  String ip;
+		if (session==null) {
+			user=null;
+			ip=null;
+		}else {
+			user = (User) session.getAttribute("user");
+			ip = request.getRemoteAddr();
+		}
 		  if (user!=null) {
 			  log.info(" IP:"+ip+" 工号:"+user.getUid()+" 操作:" +title+":"+message);
+		  }else{
+			  log.error(" IP:"+ip+"  操作:" +title+":"+message);
 		  }
 	  }
 	  
 	  public  void logError(String title,String message){
-		  User user=(User) session.getAttribute("user");
-		  String ip = request.getRemoteAddr();
+		  User user;
+		  String ip;
+		if (session==null) {
+			user=null;
+			ip=null;
+		}else {
+			user = (User) session.getAttribute("user");
+			ip = request.getRemoteAddr();
+		}
+		
 		  if (user!=null) {
 			  log.error(" IP:"+ip+" 工号:"+user.getUid()+" 操作:" +title+":"+message);
+		  }else{
+			  log.error(" IP:"+ip+"  操作:" +title+":"+message);
 		  }
 	  }
 }
